0c30243875b974da5aa4f8cf7eb205906cd97671
aboubakar97 <aboubakarpp@gmail.com>
2025-02-14 14:21:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ def carre(val_user):
 
 @app.route('/somme/<int:valeur1>/<int:valeur2>')
 def somme(valeur1,valeur2):
-    return "<h2>La somme de vos valeurs sont : </h2>" + str(valeur1 + valeur2)
+    return "<h2>La somme de vos valeurs est : </h2>" + str(valeur1 + valeur2)
   
 @app.route('/cnam/')
 def cnam():
