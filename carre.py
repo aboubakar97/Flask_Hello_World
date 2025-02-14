ac3f97a222eb5e351d89c9c399017cb1e2fb29ad
aboubakar97 <aboubakarpp@gmail.com>
2025-02-14 13:39:14 +0100

    Create carre.py

---
+++ carre.py
@@ -0,0 +1,8 @@
+# Demande à l'utilisateur de saisir un nombre
+valeur = float(input("Veuillez saisir un nombre : "))
+
+# Calcul du carré de la valeur
+carre = valeur ** 2
+
+# Affichage du résultat
+print(f"Le carré de {valeur} est {carre}.")
