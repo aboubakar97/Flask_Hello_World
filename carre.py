e7a7accbd2659a48adfb79da899bcd328a16cf1a
aboubakar97 <aboubakarpp@gmail.com>
2025-02-14 13:46:15 +0100

    Delete carre.py

--- carre.py
+++
@@ -1,8 +0,0 @@
-# Demande à l'utilisateur de saisir un nombre
-valeur = float(input("Veuillez saisir un nombre : "))
-
-# Calcul du carré de la valeur
-carre = valeur ** 2
-
-# Affichage du résultat
-print(f"Le carré de {valeur} est {carre}.")
